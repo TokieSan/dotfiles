746965f33192f14e0d591a2a4f236e67faa7e8eb
Ahmed Eltokhy <eltokhy@eltok.edu>
2021-02-28 21:21:39 +0200

    280221

--- .cache/wal/colors-wal-dwm.h
+++ .cache/wal/colors-wal-dwm.h
@@ -1,10 +1,10 @@
 static const char norm_fg[] = "#c0c0c0";
 static const char norm_bg[] = "#0b0c0c";
-static const char norm_border[] = "#868686";
+static const char norm_border[] = "#444444";
 
 static const char sel_fg[] = "#c0c0c0";
 static const char sel_bg[] = "#239D82";
-static const char sel_border[] = "#c0c0c0";
+static const char sel_border[] = "#c3c3c3";
 
 
 static const char *colors[][3]      = {
