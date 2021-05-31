4a38f3677bc8d645f53b8ef3e32a69bad9e60a57
TokieSan <elt0khy@protonmail.com>
2021-06-01 00:39:09 +0200

    1621

--- .shoosh/ali.sh
+++ .shoosh/ali.sh
@@ -1,36 +1,12 @@
 . /home/elt0khy/.shoosh/private.sh
-
-alias fucking='sudo'
-alias pls='sudo "$BASH" -c "$(history -p !!)"'
-alias viminstall='vim +PluginInstall'
-alias pauseplay='dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause'
-alias previousmusic='dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous'
-alias nextmusic='dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next'
-alias flashcard='anki'
-alias printerconfig='system-config-printer'
-alias rm='trash-put'
-alias pulsuspendrestore='systemctl restart --user pulseaudio'
-alias p='sudo pacman'
-alias du='du -h'
-alias gitadd='git add -f'
-alias gitpush='git push origin main'
-alias gitcommit='git commit -m'
-alias kilall='killall'
-alias quitX='pkill -15 Xorg'
-alias rcrdffmpeg='ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0 -f pulse -ac 2 -i default'
-alias ll='ls -all'
-alias df='df -h'
-alias deemix='deemix --bitrate FLAC -p .'
-alias harwest='~/.local/bin/harwest'
-alias rgrep='rga'
-
 rmtests() {
 	mv *test* ~/myBlackHole/tests/
 	rm -rf *.out
 	mv *.pdf ~/myBlackHole/pdf/
 	mv *.cpp ~/myBlackHole/cpp/
 	mv *.png ~/myBlackHole/png/
-	mv *.jpg *.jpeg ~/myBlackHole/jpg
+	mv *.jpg *.jpeg ~/myBlackHole/jpg/
+	mv *.mp4 *.mkv ~/myBlackHole/videos/
 }
 getAudio() {
 	ffmpeg -i "${1}" -vn -q:a 0 "${1}".ogg
@@ -98,5 +74,30 @@ alias sv='sudo vim'
 alias tunnelfast='sudo sshuttle --method=tproxy --remote=eltokhy@52.188.119.232 0.0.0.0/0 ::/0 --exclude=213.181.225.44 --exclude=52.188.119.232'
 fastHotspot() { nmcli dev wifi hotspot ifname wlan0 ssid GNUHotspot password ${hotspotPwd}; }
 export -f fastHotspot
+
 alias code='codium'
 alias weather='curl ping.gg'
+alias epic='rare'
+alias fucking='sudo'
+alias pls='sudo "$BASH" -c "$(history -p !!)"'
+alias viminstall='vim +PluginInstall'
+alias pauseplay='dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause'
+alias previousmusic='dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous'
+alias nextmusic='dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next'
+alias flashcard='anki'
+alias printerconfig='system-config-printer'
+alias rm='trash-put'
+alias pulsuspendrestore='systemctl restart --user pulseaudio'
+alias p='sudo pacman'
+alias du='du -h'
+alias gitadd='git add -f'
+alias gitpush='git push origin main'
+alias gitcommit='git commit -m'
+alias kilall='killall'
+alias quitX='pkill -15 Xorg'
+alias rcrdffmpeg='ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0 -f pulse -ac 2 -i default'
+alias ll='ls -all'
+alias df='df -h'
+alias deemix='deemix --bitrate FLAC -p .'
+alias harwest='~/.local/bin/harwest'
+alias rgrep='rga'
